1a2ddc1f3b7115744ac7a18da33e595acf41f6dc
zcm <2201668984@qq.com>
2020-11-08 09:56:45 +0800

    Signed-off-by: zcm <2201668984@qq.com>

---
+++ work3/precache-manifest.389b5ed261481283849465059d0e5b99.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96312844676b1811a737f6543911142",
+    "url": "/React/work3/index.html"
+  },
+  {
+    "revision": "b2ed611f60dac64935fb",
+    "url": "/React/work3/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b5e9dd8447c1e2d6db9c",
+    "url": "/React/work3/static/css/main.19472cf1.chunk.css"
+  },
+  {
+    "revision": "b2ed611f60dac64935fb",
+    "url": "/React/work3/static/js/2.867def74.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "/React/work3/static/js/2.867def74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e9dd8447c1e2d6db9c",
+    "url": "/React/work3/static/js/main.d82c8319.chunk.js"
+  },
+  {
+    "revision": "ee4c9aa5e46a9acca45c",
+    "url": "/React/work3/static/js/runtime-main.b6d9897c.js"
+  },
+  {
+    "revision": "60b4732520a566884758b18750c9bbf8",
+    "url": "/React/work3/static/media/1.60b47325.jpg"
+  },
+  {
+    "revision": "71214b2b44ab8ae1314f0f89701366bd",
+    "url": "/React/work3/static/media/2.71214b2b.jpg"
+  },
+  {
+    "revision": "fb6da3f5586e6e031a175a454ccfc324",
+    "url": "/React/work3/static/media/3.fb6da3f5.jpg"
+  },
+  {
+    "revision": "5b3960041be1ee18791045059c8a0e56",
+    "url": "/React/work3/static/media/4.5b396004.jpg"
+  },
+  {
+    "revision": "82e95ed892160c3e4718db754677bfcc",
+    "url": "/React/work3/static/media/5.82e95ed8.jpg"
+  },
+  {
+    "revision": "30b46cd9e1d9fcbd450b7bd219acb9f3",
+    "url": "/React/work3/static/media/6.30b46cd9.jpg"
+  },
+  {
+    "revision": "97192244b160012939b48d72f29bdd1a",
+    "url": "/React/work3/static/media/7.97192244.jpg"
+  },
+  {
+    "revision": "68dae5d4a4bdc3b52d14bfa7df2bb827",
+    "url": "/React/work3/static/media/8.68dae5d4.jpg"
+  }
+]);
